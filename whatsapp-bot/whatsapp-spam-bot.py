891d352789fff3649c7153ed52ab1b8a1fea68e1
exvynai <vir200319@gmail.com>
2022-06-05 23:24:50 +0530

    added no. of times feature

--- whatsapp-bot/whatsapp-spam-bot.py
+++ whatsapp-bot/whatsapp-spam-bot.py
@@ -3,6 +3,7 @@ import time
 
 username = input("Enter the username: ")
 msg = input("Enter the message: ")
+times = input("Enter the number of times to spam(type 'infinite' if you want to spam forever): ")
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -13,11 +14,22 @@ time.sleep(30)
 user = driver.find_element_by_xpath(f'//span[@title="{username}"]')
 user.click()
 
-while True:
-    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
-    msg_box.send_keys(msg)
+if times == 'infinite':
+    while True:
+        msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+        msg_box.send_keys(msg)
 
-    send = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
-    send.click()
+        send = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
+        send.click()
+else:
+    for i in range(int(times)):
+        msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+        msg_box.send_keys(msg)
+
+        send = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
+        send.click()
+
+
+
 
 
